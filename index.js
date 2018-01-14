9791b31440aa9e2ab4578f39bf40b5eead45252d
Oleksandr Khotemskyi <xotabu4@gmail.com>
2018-01-14 18:39:47 +0200

    Updated code to be same on git and on AWS
    Updated code to be same on git and on AWS

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
-// let run = require('./dist/aws_run.js').run
-
-// run('Suite #1 Test #1')
-
-console.time('total')
-var Mocha = require('mocha')
-var fs = require('fs')
-var path = require('path')
-var request = require('request-promise-native')
-
-var mocha = new Mocha();
-
-//mocha.ui('aws-ui');
-
-var testDir = './test/'
-// Add each .js file to the mocha instance
-fs.readdirSync(testDir).filter(function (file) {
-    // Only keep the .js files
-    return file.substr(-3) === '.js';
-}).forEach(function (file) {
-    mocha.addFile(
-        path.join(testDir, file)
-    )
-})
-
-mocha.loadFiles()
-
-let testResps = []
-for (let suite of mocha.suite.suites) {
-    for (let test of suite.tests) {
-        let testName = suite.title + ' ' + test.title
-        
-        testResps.push(request({
-            url: 'https://jbmu0byt9f.execute-api.us-west-2.amazonaws.com/prod',
-            qs: {
-                "testName": testName
-            }
-        }))
-
-        console.log(testName, ' Pushed!')
-        // mocha.run(function (failures) {
-        //     process.on('exit', function () {
-        //         console.timeEnd('total')
-        //         process.exit(failures);  // exit with non-zero status if there were failures
-        //     })
-        // })
-    }
-}
-
-Promise.all(testResps).then(function (results) {
-    console.dir(results)
-    console.timeEnd('total')
-}, function (errors) {
-    console.log(JSON.stringify(errors))
-})
+"use strict"
+// This handler will be run as AWS lambda function
+exports.handler = (event, context, callback) => {
+    var testName = event.queryStringParameters['testName']
+    var run = require('./aws_run.js').run
+    run(testName, function (failures) {
+        var response = {
+            "statusCode": 200,
+            "headers": {
+                "my_header": "my_value"
+            },
+            "body": JSON.stringify({ 'Test Name': testName, 'isFailed': failures > 0 ? true : false, "queryParams": event.queryStringParameters }),
+            "isBase64Encoded": false
+        };
+        callback(null, response);
+    })
+};
---
+++ index_local.js
@@ -0,0 +1,52 @@
+console.time('total')
+var Mocha = require('mocha')
+var fs = require('fs')
+var path = require('path')
+var request = require('request-promise-native')
+
+var mocha = new Mocha();
+var testDir = './test/'
+
+// Add each .js file to the mocha instance
+fs.readdirSync(testDir).filter(function (file) {
+    // Only keep the .js files
+    return file.substr(-3) === '.js';
+}).forEach(function (file) {
+    mocha.addFile(
+        path.join(testDir, file)
+    )
+})
+
+mocha.loadFiles()
+
+/**
+ * Files are loaded but we do not run them here. 
+ * Instead we iterate thru all parsed tests, grab their names
+ * and push test name as a parameter to same code deployed to AWS lambda
+ * this parameter will be used as mocha GREP parameter to run only 1 test for each lambda instance.
+ * 
+ */
+let testResps = []
+for (let suite of mocha.suite.suites) {
+    for (let test of suite.tests) {
+        // to grep by full describe + it name
+        let testName = suite.title + ' ' + test.title
+        
+        testResps.push(request({
+            url: 'https://jbmu0byt9f.execute-api.us-west-2.amazonaws.com/prod',
+            qs: {
+                "testName": testName
+            }
+        }))
+        console.log(testName, ' is pushed to AWS lambda!')
+    }
+}
+
+Promise.all(testResps).then(function (results) {
+    console.dir(results)
+    console.timeEnd('total')
+}, function (errors) {
+    console.log('Oh no, got errors!')
+    console.log(JSON.stringify(errors))
+    console.timeEnd('total')
+})
